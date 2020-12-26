24ea389088ba88262fd90156bb2f08623f4223b4
James <james8900@protonmail.com>
2020-12-26 14:04:35 +0000

    first commit

---
+++ app/views.py
@@ -0,0 +1,47 @@
+
+from django.shortcuts import render
+import markdown
+from pathlib import Path
+from django.shortcuts import redirect
+
+def index(request, page):
+
+    pages = [
+        'about',
+        'start',
+        'why',
+        'comfort',
+        'legs',
+        'hands',
+        'back',
+        'chest',
+        'shoulders',
+        'head',
+        'exercises',
+        'sources',
+        'contact',
+    ]
+
+    try:
+
+        pages.index(page)
+
+    except:
+
+        return redirect('/')
+
+    else:
+
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        path = str(BASE_DIR) + '/templates/pages/' + page + '.mkd' 
+
+        mkd_file = open(path, 'r', encoding='utf-8')
+        mkd = mkd_file.read()
+        html = markdown.markdown(mkd)
+
+        context = {
+            'page': page,
+            'html': html,
+            }
+
+        return render(request, 'layout.html', context)
