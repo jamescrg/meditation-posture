0174f90d068176d369d57027b616fa45c479f433
James <james8900@protonmail.com>
2020-12-27 12:57:59 +0000

    moved secret key to settings_local and changed secret key

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g4f0ob6x%81w3k9hbjgcz(ox5#*-=pb*&j9#^2bcl!u99_8@6s'
+SECRET_KEY = settings_local.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings_local.DEBUG
